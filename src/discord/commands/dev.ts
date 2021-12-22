ad7aad6a94b5c0344799020565ada67841eea8f4
Jonas Claes <jonas@jonasclaes.be>
2021-12-22 15:35:58 +0100

    React test

--- src/discord/commands/dev.ts
+++ src/discord/commands/dev.ts
@@ -8,11 +8,16 @@ export default {
     async handle(interaction: Interaction) {
         if (!interaction.isCommand()) return;
 
-        await interaction.reply({
+        const reply = await interaction.reply({
             content: "Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan op deze link meer informatie over hem vinden: <https://jonasclaes.be>\nJe kan mijn bits en bytes op deze link vinden: <https://github.com/friendshipbubble/discord-bot>",
             allowedMentions: {
                 parse: []
-            }
+            },
+            fetchReply: true
         });
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await reply.react(":smile:");
     }
 };
