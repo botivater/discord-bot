e40b08d3e997f896fa926540f38e582cbdcaa1c9
Jonas Claes <jonas@jonasclaes.be>
2021-12-22 22:10:35 +0100

    Prettier

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -16,139 +16,135 @@ import findafriend from "./commands/findafriend";
 import whatareyouthinkingabout from "./commands/whatareyouthinkingabout";
 
 export default class Discord {
-    // eslint-disable-next-line no-use-before-define
-    protected static instance: Discord | null = null;
+  // eslint-disable-next-line no-use-before-define
+  protected static instance: Discord | null = null;
 
-    protected client: Client;
+  protected client: Client;
 
-    constructor() {
-        logger.info("Discord bot is starting up.");
+  constructor() {
+    logger.info("Discord bot is starting up.");
 
-        this.client = new Client({
-            intents: [
-                Intents.FLAGS.GUILDS,
-                Intents.FLAGS.GUILD_MEMBERS,
-                Intents.FLAGS.GUILD_PRESENCES
-            ],
-        });
+    this.client = new Client({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+      ],
+    });
 
-        this.registerCommands();
+    this.registerCommands();
 
-        this.client.once("ready", Discord.handleReadyEvent);
-        this.client.on("interactionCreate", Discord.handleInteractionCreate);
+    this.client.once("ready", Discord.handleReadyEvent);
+    this.client.on("interactionCreate", Discord.handleInteractionCreate);
 
-        this.client.login(process.env.BOT_TOKEN);
+    this.client.login(process.env.BOT_TOKEN);
+  }
+
+  protected static async handleReadyEvent() {
+    const client = Discord.getInstance().client;
+
+    const channel = client.channels.cache.get(Config.getSystemChannelId());
+
+    if (channel && channel.isText()) {
+      channel.send(`Miauw! Revision: ${inlineCode(Config.getRevisionId())}`);
     }
 
-    protected static async handleReadyEvent() {
-        const client = Discord.getInstance().client;
+    client?.user?.setPresence({
+      activities: [{ name: "Lauri", type: "LISTENING" }],
+    });
 
-        const channel = client.channels.cache.get(
-            Config.getSystemChannelId()
-        );
+    syncAllUsersInAllGuilds(client);
 
-        if (channel && channel.isText()) {
-            channel.send(`Miauw! Revision: ${inlineCode(Config.getRevisionId())}`);
-        }
+    setInterval(() => {
+      const discordClient = Discord.getInstance().client;
 
-        client?.user?.setPresence({
-            activities: [{ name: "Lauri", type: "LISTENING" }],
-        });
+      syncAllUsersInAllGuilds(discordClient);
+    }, 60000);
 
-        syncAllUsersInAllGuilds(client);
+    logger.info("Discord bot is ready.");
+  }
 
-        setInterval(() => {
-            const discordClient = Discord.getInstance().client;
+  protected static async handleInteractionCreate(
+    interaction: Interaction<CacheType>
+  ) {
+    if (!interaction.isCommand()) return;
 
-            syncAllUsersInAllGuilds(discordClient);
-        }, 60000);
+    const { commandName } = interaction;
 
-        logger.info("Discord bot is ready.");
+    switch (commandName) {
+      case ping.command.name:
+        await ping.handle(interaction);
+        break;
+
+      case cat.command.name:
+        await cat.handle(interaction);
+        break;
+
+      case findafriend.command.name:
+        await findafriend.handle(interaction);
+        break;
+
+      case selfcare.command.name:
+        await selfcare.handle(interaction);
+        break;
+
+      case whatareyouthinkingabout.command.name:
+        await whatareyouthinkingabout.handle(interaction);
+        break;
+
+      case toneindicator.command.name:
+        await toneindicator.handle(interaction);
+        break;
+
+      case dev.command.name:
+        await dev.handle(interaction);
+        break;
+
+      default:
+        break;
     }
+  }
 
-    protected static async handleInteractionCreate(
-        interaction: Interaction<CacheType>
-    ) {
-        if (!interaction.isCommand()) return;
+  protected registerCommands() {
+    const commands = [
+      ping.command,
+      cat.command,
+      findafriend.command,
+      selfcare.command,
+      whatareyouthinkingabout.command,
+      toneindicator.command,
+      dev.command,
+    ].map((command) => command.toJSON());
 
-        const { commandName } = interaction;
+    // Register commands
+    const rest = new REST({ version: "9" }).setToken(Config.getBotToken());
 
-        switch (commandName) {
-            case ping.command.name:
-                await ping.handle(interaction);
-                break;
+    // Globally register commands
+    rest.put(Routes.applicationCommands(Config.getApplicationId()), {
+      body: [],
+    });
 
-            case cat.command.name:
-                await cat.handle(interaction);
-                break;
+    // Prod server commands
+    // TODO: Remove me!
+    rest.put(
+      Routes.applicationGuildCommands(
+        Config.getApplicationId(),
+        "803327192662671463"
+      ),
+      {
+        body: commands,
+      }
+    );
+  }
 
-            case findafriend.command.name:
-                await findafriend.handle(interaction);
-                break;
+  public static getInstance() {
+    if (this.instance) return this.instance;
 
-            case selfcare.command.name:
-                await selfcare.handle(interaction);
-                break;
+    this.instance = new Discord();
+    return this.instance;
+  }
 
-            case whatareyouthinkingabout.command.name:
-                await whatareyouthinkingabout.handle(interaction);
-                break;
-
-            case toneindicator.command.name:
-                await toneindicator.handle(interaction);
-                break;
-
-            case dev.command.name:
-                await dev.handle(interaction);
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    protected registerCommands() {
-        const commands = [
-            ping.command,
-            cat.command,
-            findafriend.command,
-            selfcare.command,
-            whatareyouthinkingabout.command,
-            toneindicator.command,
-            dev.command,
-        ].map((command) =>
-            command.toJSON()
-        );
-
-        // Register commands
-        const rest = new REST({ version: "9" }).setToken(Config.getBotToken());
-
-        // Globally register commands
-        rest.put(Routes.applicationCommands(Config.getApplicationId()), {
-            body: [],
-        });
-
-        // Prod server commands
-        // TODO: Remove me!
-        rest.put(
-            Routes.applicationGuildCommands(
-                Config.getApplicationId(),
-                "803327192662671463"
-            ),
-            {
-                body: commands,
-            }
-        );
-    }
-
-    public static getInstance() {
-        if (this.instance) return this.instance;
-
-        this.instance = new Discord();
-        return this.instance;
-    }
-
-    public getClient(): Client {
-        return this.client;
-    }
+  public getClient(): Client {
+    return this.client;
+  }
 }
