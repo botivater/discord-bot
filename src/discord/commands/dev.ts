c65d2f6069d025b5889cc40e2c4cd1514aa664b8
Jonas Claes <jonas@jonasclaes.be>
2021-12-22 15:49:51 +0100

    React test

--- src/discord/commands/dev.ts
+++ src/discord/commands/dev.ts
@@ -1,5 +1,5 @@
 import { hideLinkEmbed, hyperlink, SlashCommandBuilder } from "@discordjs/builders";
-import { Interaction } from "discord.js";
+import { Interaction, Message } from "discord.js";
 
 export default {
     command: new SlashCommandBuilder()
@@ -8,7 +8,7 @@ export default {
     async handle(interaction: Interaction) {
         if (!interaction.isCommand()) return;
 
-        const reply = await interaction.reply({
+        const message = await interaction.reply({
             content: "Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan op deze link meer informatie over hem vinden: <https://jonasclaes.be>\nJe kan mijn bits en bytes op deze link vinden: <https://github.com/friendshipbubble/discord-bot>",
             allowedMentions: {
                 parse: []
@@ -16,8 +16,8 @@ export default {
             fetchReply: true
         });
 
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        await reply.react(":smile:");
+        if (message instanceof Message) {
+            await message.react(":smile:");
+        }
     }
 };
