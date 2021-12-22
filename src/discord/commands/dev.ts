9732240649d613453ea5bfde3801c9fdb0faab8b
Jonas Claes <jonas@jonasclaes.be>
2021-12-22 15:17:57 +0100

    Links

--- src/discord/commands/dev.ts
+++ src/discord/commands/dev.ts
@@ -8,11 +8,8 @@ export default {
     async handle(interaction: Interaction) {
         if (!interaction.isCommand()) return;
 
-        const devLink = hyperlink("hier", "https://jonasclaes.be");
-        const repoLink = hyperlink("hier", "https://github.com/friendshipbubble/discord-bot");
-
         await interaction.reply({
-            content: `Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan ${devLink} meer informatie over hem vinden.\nJe kan mijn bits en bytes ${repoLink} vinden!`,
+            content: "Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan op deze link meer informatie over hem vinden: <https://jonasclaes.be>\nJe kan mijn bits en bytes op deze link vinden: <https://github.com/friendshipbubble/discord-bot>",
             allowedMentions: {
                 parse: []
             }
