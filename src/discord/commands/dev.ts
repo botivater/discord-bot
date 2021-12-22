722166dc4d76206ad8b552de4658f23604f13f3d
Jonas Claes <jonas@jonasclaes.be>
2021-12-22 15:14:35 +0100

    Oopsie broke links

--- src/discord/commands/dev.ts
+++ src/discord/commands/dev.ts
@@ -9,12 +9,10 @@ export default {
         if (!interaction.isCommand()) return;
 
         const devLink = hyperlink("hier", "https://jonasclaes.be");
-        const devLinkHideEmbed = hideLinkEmbed(devLink);
         const repoLink = hyperlink("hier", "https://github.com/friendshipbubble/discord-bot");
-        const repoLinkHideEmbed = hideLinkEmbed(repoLink);
 
         await interaction.reply({
-            content: `Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan ${devLinkHideEmbed} meer informatie over hem vinden.\nJe kan mijn bits en bytes ${repoLinkHideEmbed} vinden!`,
+            content: `Hey!\nIk ben Mira, en ik ben de kat van Lauri!\n<@487283576325275648> heeft mij speciaal gemaakt voor de Friendship Bubble en onderhoudt mij dagelijks!\nJe kan ${devLink} meer informatie over hem vinden.\nJe kan mijn bits en bytes ${repoLink} vinden!`,
             allowedMentions: {
                 parse: []
             }
