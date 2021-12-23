a269224c18a4a664af2d4f51d5ff2a49b53844b3
Jonas Claes <jonas@jonasclaes.be>
2021-12-23 11:40:55 +0100

    Removed old cat implementation

--- src/discord/commands/cat.ts
+++ src/discord/commands/cat.ts
@@ -12,18 +12,5 @@ export default {
     await interaction.reply(
       "Nieuwe wijsheid van mij:\n/cat is nu /vindeenvriendje! Take a look!\nMiauw out!"
     );
-
-    // try {
-    //     await interaction.deferReply();
-
-    //     setTimeout(async () => {
-    //         const randomNumber = Math.floor(Math.random() * 57);
-
-    //         await interaction.editReply({ files: [`https://static.friendshipbubble.nl/mira/cats/${randomNumber}.jpg`] });
-    //     }, 1000);
-    // } catch (e) {
-    //     logger.error(e);
-    //     await interaction.editReply("Miauw! Er is een fout opgetreden!");
-    // }
   },
 };
